7c44e97011e3e159f9fcd49c13f552e9a4722db2
Your Name <you@example.com>
2025-04-11 19:19:31 -0300

    novas adições

--- Calculadora Simples.py
+++ Calculadora Simples.py
@@ -7,10 +7,14 @@ num2   = 0
 result = 0
 op     = ""
 
+# Loop infinito para continuar pedindo entradas até que o usuário decida sair
+
 while True:
     num1 = float(input("Digite o primeiro número: ")) # ler o primeiro numero
     op   = input("Digite a operação (+, -, *, /): ") # ler a operação desejada
     num2 = float(input("Digite o segundo número: ")) # ler o segundo numero
+
+# Condições para verificar a operação desejada e realizar o cálculo correspondente
 
     if op == '+':
         result = num1 + num2
@@ -18,7 +22,12 @@ while True:
         result = num1 - num2
     elif op == '*':
         result = num1 * num2
-    elif op == '/':
+    elif op == '/': # Verifica se o segundo número é zero antes de realizar a divisão
+        try:
+            result = num1 / num2
+        except ZeroDivisionError: # Tratamento de exceção para divisão por zero
+            print("Erro: Divisão por zero não é permitida.")
+            continue
         result = num1 / num2
     else:
         print("Operação inválida!")
